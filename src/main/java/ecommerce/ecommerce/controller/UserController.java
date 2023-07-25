0414d86089884c3a80ea22c5e011557b3888a36e
ch3lla <chukwumaemmanuella03@gmail.com>
2023-07-25 14:56:34 +0100

    made some changes

--- src/main/java/ecommerce/ecommerce/controller/UserController.java
+++ src/main/java/ecommerce/ecommerce/controller/UserController.java
@@ -1,35 +1,39 @@
 package ecommerce.ecommerce.controller;
 
+import ecommerce.ecommerce.model.Product;
 import ecommerce.ecommerce.model.User;
+import ecommerce.ecommerce.repository.ProductRepository;
+import ecommerce.ecommerce.service.ProductService;
 import ecommerce.ecommerce.service.UserService;
 import lombok.AllArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/api/v1/users")
+@Controller
+@RequestMapping("/api/v1")
 @AllArgsConstructor
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping
+    @GetMapping("/users")
     public ResponseEntity<List<User>> allUsers(){
         return new ResponseEntity<>(userService.allUsers(), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/users/{id}")
     public ResponseEntity<Optional<User>> getSingleUser (@PathVariable ObjectId id){
         return  new ResponseEntity<>(userService.findById(id), HttpStatus.OK);
     }
 
-    @PostMapping
+    @PostMapping("/users")
     public ResponseEntity<User> createUser(@RequestBody User user){
         try {
             User newUser = userService.createUser(user);
@@ -39,7 +43,7 @@ public class UserController {
         }
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/users/{id}")
     public ResponseEntity<User> updateUser(@PathVariable ObjectId id, @RequestBody User updateUser){
         try {
             User updateUserData = userService.updateUser(id, updateUser);
@@ -49,9 +53,30 @@ public class UserController {
         }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/users/{id}")
     public ResponseEntity<User> deleteUser (@PathVariable ObjectId id){
         userService.deleteUser(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> allProducts () {
+        return new ResponseEntity<>(productService.allProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/products/{productName}")
+    public ResponseEntity<Optional<Product>> getSingleProduct ( @PathVariable String productName) {
+        return new ResponseEntity<>(productService.getSingleProduct(productName), HttpStatus.OK);
+    }
+
+    @GetMapping("/products/brand/{productBrand}")
+    public ResponseEntity<List<Product>> getProductBrand (@PathVariable String productBrand){
+        return new ResponseEntity<List<Product>>(productService.getProductBrand(productBrand), HttpStatus.OK);
+    }
+
 }
